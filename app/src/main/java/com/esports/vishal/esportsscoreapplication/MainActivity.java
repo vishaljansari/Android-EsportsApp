0e0166bdf6866ff0992c46b8fe3e1e1eafb27c7d
VISHAL JANSARI <vishaljansari@hotmail.com>
2017-07-12 20:00:19 -0700

    initial commit

---
+++ app/src/main/java/com/esports/vishal/esportsscoreapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package com.esports.vishal.esportsscoreapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+        private static int SPLASH_TIMEOUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
