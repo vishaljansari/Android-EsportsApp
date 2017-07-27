263fae3a2fab90c68dda23c4621d590777b155a5
sagarvadher <sagar@sagars-MacBook-Pro.local>
2017-07-26 21:22:54 -0700

    youtubedemo2 working now with any given youtube video

---
+++ youtubedemo2/app/src/main/java/com/example/sagar/youtubedemo/Config.java
@@ -0,0 +1,12 @@
+package com.example.sagar.youtubedemo;
+
+/**
+ * Created by sagar on 7/26/17.
+ */
+
+public class Config {
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyDvNQnyRnCicQfgkyg7U6uQ6rbGzG-5HCs";
+}
